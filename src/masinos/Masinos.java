7e347b543132179c446770854f590d777bf9179a
rugile <rugile.geceviciute@gmail.com>
2021-01-13 14:33:12 +0200

    cars game done

--- src/masinos/Masinos.java
+++ src/masinos/Masinos.java
@@ -3,7 +3,7 @@ package masinos;
 
 public class Masinos {
      //nuvaziuotas kelias
-    //kiekvieno ciklo metu visos masinos turi pavaziuoti nuo tarp 1 ir 10 km random
+    //kiekvieno ciklo metu visos masinos turi pavaziuoti nuo tarp 1 ir 1000 km random
     //lenktynes baigiasi kai bent viena nuvaziuoja 1000 km
     //kai baigias lenktynes masinos surusuojamos nuvaziuoto kelio mazejimo tvarka
     //toliausiai nuvaziavus, sarasas kiek kuri nuvaziavo kai yra pasiekta 1000km
@@ -11,24 +11,33 @@ public class Masinos {
     
     public static void main(String[] args) {
        int[] masinos = new int [8];
-       int pirmaujanti;
+       boolean doRace = true;
+       while(doRace){
+        //cars moves 10km
         for(int i = 0; i < masinos.length; i++) {
-            masinos[i] = (int) (Math.random() * 1000 + 1);
-            System.out.print(masinos[i] + " ");
+            masinos[i] += Math.random() * 10 + 1;
         }
-        for(int i =0; i < masinos.length - 1; i++) {
-            for(int j = i+1; j < masinos.length; j++) {
-                if(masinos[i] < masinos[j]) {
-                    pirmaujanti = masinos[i];
-                    masinos[i] = masinos[j];
-                    masinos [j] = pirmaujanti;
-                }
+        //ar nuline pasieke 1000km? visas tikrina
+        for(int i = 0; i < masinos.length; i++) {
+            if (masinos[i] >= 1000) {
+                System.out.println("Laimejo " + (i+1)+" masina");
+                doRace = false;
+                break;
             }
         }
-        System.out.println(" ");
-        System.out.println("LAIMETOJA");
-        for (int i=0; i < masinos.length; i++) {
-            System.out.println(masinos[i] + " ");
+    } 
+       //rusiavimas
+        for(int i = 0; i < masinos.length - 1; i++) {
+            for(int j = i+1; j < masinos.length; j++) {
+                if(masinos[i] < masinos[j]) {
+                        int pirmaujanti = masinos[i];
+                        masinos[i] = masinos[j];
+                        masinos [j] = pirmaujanti;
+                    }
+                }  
+            }
+        for(int i = 0; i < masinos.length; i++) {
+            System.out.println(masinos[i]);
         }
     }
     
