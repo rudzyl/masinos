e9e03e039e62d5d00ea6f26cb2b772cae12f8038
rugile <rugile.geceviciute@gmail.com>
2021-01-16 11:11:10 +0200

    race with class

---
+++ src/masinos/Masina.java
@@ -0,0 +1,46 @@
+
+package masinos;
+
+public class Masina {
+    
+    //private 
+    public String pav;
+    //private 
+    public int maxGreitis; //turis
+    //private 
+    public int kelias; //kiekis
+    //private 
+    public int greitis;
+    
+    public Masina(String pav, int maxGreitis) {
+        this. pav = pav;
+        this. maxGreitis = maxGreitis; //negali virsyti maxGreitis 
+        this.kelias = 0;
+        this.greitis = 0;//neigiamas greitis buti negali
+    }
+    /*
+    matodas
+    void gazuok(int kiek)
+    voidstabdyk(int kiek)
+    void vaziuok() kai iskvieciamas metodas vaziuok pavaziuoja kelias padideja per tiek koks yra masinos greitis 
+    jei greitis 25 nuvaziuos 25km
+    
+    sakom if( Math.random > 0.3) stabdys
+    if else (Math.random > tarp 0.3 ir 0.8 gazuos
+    else nekeis greicio
+    30% tikimybe kad stabdys per 1-5 tikimybe
+    50% tikimybe kad gazuos per 1-10
+    20% tikimybe kad nekeis greicio
+    jei sportine tai 
+    Math.random > 0.5 spoilerio pozicija pasikei padarys nuleista arba pakels
+    50% tikimybe kad pasikeis spolerio pozicija
+    */
+    /*
+    SportineMasina
+    ji turi spoileri, jei true pakeltas false nuleistas
+    grazuok()jei metode jei spoileris nuleistas - gazuoja 2 kart greiciau
+    stabdyk()jei spoileris pakeltas - stabdo 2kartus greiciau
+    
+    
+    */
+}
--- src/masinos/Masinos.java
+++ src/masinos/Masinos.java
@@ -4,31 +4,39 @@ package masinos;
 public class Masinos {
     
     public static void main(String[] args) {
-       int[] masinos = new int [8];
+        Masina m1 = new Masina("Pirma", 140);
+        Masina m2 = new Masina("Antra", 160);
+        Masina m3 = new Masina("Trecia", 180);
+        Masina m4 = new Masina("Ketvirta", 200);
+        Masina[] race = {m1, m2, m3, m4};
+//            new SportineMasina("spr1", 300)
+//            new SportineMasina("spr2",300)
+        
+       //int[] masinos = new int [8];//objektai masinos negali but int 
        //milestone kas 100km issausdina 
        int interm = 100;
        boolean doRace = true;
        while(doRace){
         //cars moves 10km
-            for(int i = 0; i < masinos.length; i++) {
-                masinos[i] += Math.random() * 10 + 1;
+            for(int i = 0; i < race.length; i++) {
+                race[i] += Math.random() * 10 + 1;
             }
             //isspausdinimas kas 100km kiek kuri nuvaziavo
             boolean printInterm = false;
-            for(int i = 0; i < masinos.length; i++) {
+            for(int i = 0; i < race.length; i++) {
                 if(masinos[i] >= interm) {
                     printInterm = true;
                     interm += 100;
                 }
             }
             if(printInterm) {
-                for(int i = 0; i < masinos.length; i++) {
+                for(int i = 0; i < race.length; i++) {
                     System.out.print(masinos[i] + "\t");
                 }
                 System.out.println();
             }
             //ar nuline pasieke 1000km? visas tikrina 
-            for(int i = 0; i < masinos.length; i++) {
+            for(int i = 0; i < race.length; i++) {
                 if (masinos[i] >= 1000) {
                     System.out.println("Laimejo " + (i+1)+" masina");
                     doRace = false;
@@ -37,8 +45,8 @@ public class Masinos {
             }
         } 
        //rusiavimas nuo didziausio iki maziausio
-        for(int i = 0; i < masinos.length - 1; i++) {
-            for(int j = i+1; j < masinos.length; j++) {
+        for(int i = 0; i < race.length - 1; i++) { //reik pakeist
+            for(int j = i+1; j < race.length; j++) {
                 if(masinos[i] < masinos[j]) {
                         int pirmaujanti = masinos[i];
                         masinos[i] = masinos[j];
@@ -46,7 +54,7 @@ public class Masinos {
                     }
                 }  
             }
-        for(int i = 0; i < masinos.length; i++) {
+        for(int i = 0; i < race.length; i++) {
             System.out.println(masinos[i]);
         }
     }
